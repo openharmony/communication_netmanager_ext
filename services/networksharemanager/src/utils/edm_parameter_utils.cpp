11b5d3aadb649d13e83186092be1afbefdde1066
yujiace <yujiace@huawei.com>
2024-06-01 06:11:44 +0000

    update services/networksharemanager/src/utils/edm_parameter_utils.cpp.
    Signed-off-by: yujiace <yujiace@huawei.com>

--- services/networksharemanager/src/utils/edm_parameter_utils.cpp
+++ services/networksharemanager/src/utils/edm_parameter_utils.cpp
@@ -22,10 +22,6 @@ namespace OHOS {
 namespace NetManagerStandard {
 constexpr uint32_t PARAM_BUFFER_LENGTH = 128;
 
-public:
-EdmParameterUtils(const EdmParameterUtils&) = delete;
-EdmParameterUtils& operator=(const EdmParameterUtils&) = delete;
-
 EdmParameterUtils &EdmParameterUtils::GetInstance()
 {
     static EdmParameterUtils instance;
