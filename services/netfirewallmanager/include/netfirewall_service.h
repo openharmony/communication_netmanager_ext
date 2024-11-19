4c4d4eb1229c5894640a6adc7202188e0dd72b53
peng.nie <niepeng14@h-partners.com>
2024-11-19 08:35:26 +0800

    修改用户切换延迟加载
    Signed-off-by: peng.nie <niepeng14@h-partners.com>

--- services/netfirewallmanager/include/netfirewall_service.h
+++ services/netfirewallmanager/include/netfirewall_service.h
@@ -184,6 +184,8 @@ private:
 
     void RegisterSubscribeCommonEvent();
 
+    void UserChangeEvent(int32_t userId);
+
 private:
     static std::shared_ptr<ffrt::queue> ffrtServiceHandler_;
     std::atomic<uint64_t> currentSetRuleSecond_ = 0;
