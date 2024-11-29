a11eb747bc0412d8eab7e68bd17cf91cfa1265c1
康香娟 <kangxiangjuan1@h-partners.com>
2024-11-29 09:52:14 +0800

    响应充电状态事件接口优化重写
    Signed-off-by: 康香娟 <kangxiangjuan1@h-partners.com>

--- services/wearabledistributednetmanager/src/wearable_distributed_net_service.cpp
+++ services/wearabledistributednetmanager/src/wearable_distributed_net_service.cpp
@@ -129,16 +129,16 @@ void WearableDistributedNetService::ReceiveMessage::OnReceiveEvent(const EventFw
     const auto &chargingState = want.GetIntParam(PowerMgr::BatteryInfo::COMMON_EVENT_KEY_CHARGE_STATE,
                                                  static_cast<int32_t>(ChargeState::CHARGE_STATE_BUTT));
 
-	NETMGR_EXT_LOG_I("Wearable Distributed Net receive power message: chargingState = %{public}d", chargingState);
-	if (chargingState == static_cast<int32_t>(ChargeState::CHARGE_STATE_DISABLE) ||
+    NETMGR_EXT_LOG_I("Wearable Distributed Net receive power message: chargingState = %{public}d", chargingState);
+    if (chargingState == static_cast<int32_t>(ChargeState::CHARGE_STATE_DISABLE) ||
         chargingState == static_cast<int32_t>(ChargeState::CHARGE_STATE_FULL) ||
-		chargingState == static_cast<int32_t>(ChargeState::CHARGE_STATE_NONE)) {
+        chargingState == static_cast<int32_t>(ChargeState::CHARGE_STATE_NONE)) {
         NETMGR_EXT_LOG_I("Wearable Distributed Net receive power disconnected message");
-		WearableDistributedNetService_.UpdateNetScore(false);
-	} else if (chargingState == static_cast<int32_t>(ChargeState::CHARGE_STATE_ENABLE)) {
+        WearableDistributedNetService_.UpdateNetScore(false);
+    } else if (chargingState == static_cast<int32_t>(ChargeState::CHARGE_STATE_ENABLE)) {
         NETMGR_EXT_LOG_I("Wearable Distributed Net receive power connected message");
-		WearableDistributedNetService_.UpdateNetScore(true);
-	}
+        WearableDistributedNetService_.UpdateNetScore(true);
+    }
 }
 } // namespace NetManagerStandard
 } // namespace OHOS
