13c8ece44c38a60f2c453dfb57916e3c2f7a956d
wendan4 <wendan4@huawei.com>
2025-03-03 01:50:27 +0000

    update services/networksharemanager/src/utils/networkshare_utils.cpp.
    Signed-off-by: wendan4 <wendan4@huawei.com>

--- services/networksharemanager/src/utils/networkshare_utils.cpp
+++ services/networksharemanager/src/utils/networkshare_utils.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -20,12 +20,6 @@ namespace OHOS {
 namespace NetManagerStandard {
  
 constexpr int32_t TEN = 10;
- 
-NetworkShareUtils &NetworkShareUtils::GetInstance()
-{
-    static NetworkShareUtils instance;
-    return instance;
-}
 
 bool EdmParameterUtils::ConvertToInt64(const std::string &str, int64_t &value)
 {
