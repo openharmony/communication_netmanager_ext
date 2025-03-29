18b81f12976fc0f8c57276063e22d0814e4a98d5
HuangHaitao <huanghaitao16@huawei.com>
2025-03-29 04:33:42 +0000

    update services/vpnmanager/src/vpn_database_helper.cpp.
    Signed-off-by: HuangHaitao <huanghaitao16@huawei.com>

--- services/vpnmanager/src/vpn_database_helper.cpp
+++ services/vpnmanager/src/vpn_database_helper.cpp
@@ -18,7 +18,6 @@
 #include <cstdlib>
 #include <filesystem>
 
-#include "base64_utils.h"
 #include "net_manager_constants.h"
 #include "net_manager_ext_constants.h"
 #include "netmgr_ext_log_wrapper.h"
