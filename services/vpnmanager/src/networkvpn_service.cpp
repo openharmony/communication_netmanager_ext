40f97897fb2f9819bd8f69622cd471ae68a98c68
libaicai <baicai.li@td-tech.com>
2025-06-11 18:22:38 +0800

    根据persist参数拦截启用VPN
    Signed-off-by: libaicai <baicai.li@td-tech.com>

--- services/vpnmanager/src/networkvpn_service.cpp
+++ services/vpnmanager/src/networkvpn_service.cpp
@@ -646,7 +646,7 @@ int32_t NetworkVpnService::SetUpVpn(const VpnConfig &config, bool isVpnExtCall)
 {
     NETMGR_EXT_LOG_I("SetUpVpn in");
     std::unique_lock<std::mutex> locker(netVpnMutex_);
-    if (OHOS::system::GetParameter("persist.edm.vpn_disable", "false") != "true") {
+    if (OHOS::system::GetBoolParameter("persist.edm.vpn_disable", false)) {
         NETMGR_EXT_LOG_E("persist.edm.vpn_disable disallowed setting up vpn");
         return NETMANAGER_EXT_ERR_PERMISSION_DENIED;
     }
@@ -666,13 +666,9 @@ int32_t NetworkVpnService::SetUpVpn(const VpnConfig &config, bool isVpnExtCall)
         return ret;
     }
     if (vpnObj_ != nullptr) {
-        if (vpnObj_->GetUserId() == userId) {
-            NETMGR_EXT_LOG_W("vpn exist already, please execute destory first");
-            return NETWORKVPN_ERROR_VPN_EXIST;
-        } else {
-            NETMGR_EXT_LOG_W("vpn using by other user");
-            return NETWORKVPN_ERROR_VPN_EXIST;
-        }
+        NETMGR_EXT_LOG_W("%{public}s", (vpnObj_->GetUserId() == userId ?
+            "vpn exist already, please execute destory first" : "vpn using by other user"));
+        return NETWORKVPN_ERROR_VPN_EXIST;
     }
     vpnObj_ = std::make_shared<ExtendedVpnCtl>(configPtr, "", userId, activeUserIds);
     if (vpnObj_->RegisterConnectStateChangedCb(vpnConnCallback_) != NETMANAGER_EXT_SUCCESS) {
