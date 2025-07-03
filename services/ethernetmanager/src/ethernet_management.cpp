17ea5b9c5a4719431ee443f28c722dfd68991701
张文杰 <zhangwenjie65@h-partners.com>
2025-07-03 09:30:55 +0000

    update services/ethernetmanager/src/ethernet_management.cpp.
    Signed-off-by: 张文杰 <zhangwenjie65@h-partners.com>

--- services/ethernetmanager/src/ethernet_management.cpp
+++ services/ethernetmanager/src/ethernet_management.cpp
@@ -597,7 +597,7 @@ bool EthernetManagement::GetSysNodeValue(const std::string &nodePath, std::strin
     std::ifstream infile;
     std::string strLine;
 
-    char nodeRealpath[PATH_MAX];
+    char nodeRealpath[PATH_MAX + 1];
     if (realpath(nodePath.c_str(), nodeRealpath) == NULL) {
         NETMGR_EXT_LOG_E("GetSysNodeValue get realpath failed");
         return false;
