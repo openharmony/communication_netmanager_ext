e151e6e0e4caa67da9c16a1151a82e31c502f2aa
Zhang_Wuling <zhangsheng81@huawei.com>
2025-09-17 11:14:54 +0800

    update: 更新文件 net_vpn_impl.cpp
    Signed-off-by: Zhang_Wuling <zhangsheng81@huawei.com>

--- services/vpnmanager/src/net_vpn_impl.cpp
+++ services/vpnmanager/src/net_vpn_impl.cpp
@@ -367,7 +367,7 @@ void NetVpnImpl::DelNetLinkInfo(NetConnClient &netConnClientIns)
         AdjustRouteInfo(route);
         std::string destAddress = route.destination_.address_ + "/" + std::to_string(route.destination_.prefixlen_);
         NetsysController::GetInstance().NetworkRemoveRoute(
-            netId_, route.iface_, destAddress, route.gateway_.address_, route.isExcludeRoute_);
+            netId_, route.iface_, destAddress, route.gateway_.address_, route.isExcludedRoute_);
     }
 }
 
