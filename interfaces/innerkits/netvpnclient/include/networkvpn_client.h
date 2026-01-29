7ea9abdea07ac572240cbeb004b696b6e6804ee5
heqianmo <heqianmo@huawei.com>
2026-01-29 17:55:51 +0800

    update: 更新文件 networkvpn_client.h
    Signed-off-by: heqianmo <heqianmo@huawei.com>

--- interfaces/innerkits/netvpnclient/include/networkvpn_client.h
+++ interfaces/innerkits/netvpnclient/include/networkvpn_client.h
@@ -297,9 +297,6 @@ public:
     int32_t SetSelfVpnPid();
     void SetVpnSaState(bool state);
 
-    int32_t StartVpnExtensionAbility(AAFwk::Want want);
-    int32_t StopVpnExtensionAbility(AAFwk::Want want);
-
 private:
     class MonitorVpnServiceDead : public IRemoteObject::DeathRecipient {
     public:
