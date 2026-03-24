49f8f84005398989062768d4999241e2ee38f95f
hhd227 <huanghaodong9@h-partners.com>
2026-03-24 21:24:11 +0800

    update: 更新文件 virtual_vpn_ctl_test.cpp
    Signed-off-by: hhd227 <huanghaodong9@h-partners.com>

--- test/vpnmanager/unittest/sys_vpn_manager_test/virtual_vpn_ctl_test.cpp
+++ test/vpnmanager/unittest/sys_vpn_manager_test/virtual_vpn_ctl_test.cpp
@@ -133,9 +133,9 @@ HWTEST_F(VirtualVpnCtlTest, SetUp003, TestSize.Level1)
 
     EXPECT_CALL(*mockNetConnService, GetNetIdByIdentifier(_, _)).
         WillRepeatedly([](const std::string &ident, std::list<int32_t> &netIdList) {
-                            netIdList.push_back(123);
-                            return 0;
-                          });
+            netIdList.push_back(123);
+            return 0;
+        });
     control_->netSupplierId_ = 0;
     EXPECT_EQ(control_->SetUp(true), NETMANAGER_EXT_ERR_INTERNAL);
 
