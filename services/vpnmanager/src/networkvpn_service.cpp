cdf26e785c79438871698ed780783110d2c9a379
hhd227 <huanghaodong9@h-partners.com>
2026-05-21 20:07:14 +0800

    update: 更新文件 networkvpn_service.cpp
    Signed-off-by: hhd227 <huanghaodong9@h-partners.com>

--- services/vpnmanager/src/networkvpn_service.cpp
+++ services/vpnmanager/src/networkvpn_service.cpp
@@ -2486,6 +2486,11 @@ int32_t NetworkVpnService::RequestVpnPermission(int32_t uid, const std::string& 
     const std::string& abilityName, bool &isAuthorized)
 {
     isAuthorized = false;
+    // LCOV_EXCL_START
+ 	    if (!NetManagerPermission::IsSystemCaller()) {
+ 	        return NETMANAGER_EXT_ERR_PERMISSION_DENIED;
+ 	    }
+ 	// LCOV_EXCL_STOP
     std::string actualBundleName;
     std::string appName;
     GetAppNameByUid(uid, appName, actualBundleName);
