10474adc76dc293784a0db60fa3eaa92e2539d5f
huang-haitao <huanghaitao16@h-partners.com>
2026-06-26 11:06:10 +0800

    avoid connect dialog directly
    Signed-off-by: huang-haitao <huanghaitao16@h-partners.com>

--- frameworks/vpn_dialog/dialog_ui/vpn_dialog/src/main/ets/ServiceExtAbility/VpnDialogAbility.ts
+++ frameworks/vpn_dialog/dialog_ui/vpn_dialog/src/main/ets/ServiceExtAbility/VpnDialogAbility.ts
@@ -15,9 +15,14 @@
 
 import extension from '@ohos.app.ability.ServiceExtensionAbility';
 import window from '@ohos.window';
-import type common from '@ohos.app.ability.common';
+import osAccount from '@ohos.account.osAccount';
 import rpc from '@ohos.rpc';
 import type Want from '@ohos.app.ability.Want';
+import appManager from '@ohos.app.ability.appManager';
+import bundleManager from '@ohos.bundle.bundleManager';
+import bundleResourceManager from '@ohos.bundle.bundleResourceManager';
+import dataShare from '@ohos.data.dataShare';
+import dataSharePredicates from '@ohos.data.dataSharePredicates';
 import GlobalContext from '../MainAbility/GlobalContext';
 import display from '@ohos.display';
 
@@ -26,7 +31,8 @@ class VpnDialogStub extends rpc.RemoteObject {
     super(des);
   }
   onRemoteRequest(code, data, reply, option): boolean {
-    return true;
+    // reject all requests since it is a stub
+    return false;
   }
 }
 
@@ -46,12 +52,20 @@ export default class VpnDialogAbility extends extension {
    * Lifecycle function, called back when a service extension is started for initialization.
    */
   onCreate(want: Want): void {
-    console.info('onCreate is triggered ' + JSON.stringify(want));
-    if (want && want.parameters && want.parameters.appName) {
-      console.info('onCreate is triggered calling bundleName' + want.parameters.appName);
-      globalThis.vpnCallingAppName = want.parameters.appName;
+    console.info('onCreate is triggered');
+    if (want && want.parameters) {
+      console.info('onCreate is triggered');
       globalThis.vpnCallingPid = want.parameters['ohos.aafwk.param.callerPid'];
       globalThis.vpnCallingUid = want.parameters['ohos.aafwk.param.callerUid'];
+      try {
+        const bundleName = bundleManager.getBundleNameByUidSync(globalThis.vpnCallingUid);
+        globalThis.bundleName = bundleName;
+        const bundleResourceInfo = bundleResourceManager.getBundleResourceInfo(bundleName,
+          bundleResourceManager.ResourceFlag.GET_RESOURCE_INFO_WITH_LABEL);
+        globalThis.vpnCallingAppName = bundleResourceInfo.label;
+      } catch (err) {
+        console.error('get bundle info failed!' + JSON.stringify(err));
+      }
     }
     this.windowNum = 0;
   }
@@ -60,7 +74,7 @@ export default class VpnDialogAbility extends extension {
     console.log('onConnect is triggered');
   
     globalThis.extensionContext = this.context;
-
+    const stub = new VpnDialogStub('VpnRightDialog');
     if (want?.parameters) {
  	    globalThis.parameters = want.parameters;
  	  }
@@ -69,10 +83,74 @@ export default class VpnDialogAbility extends extension {
       globalThis.abilityName = abilityName;
     }
     if (want?.parameters?.bundleName) {
-      let bundleName: string = want.parameters.bundleName.toString();
-      globalThis.bundleName = bundleName;
+      if (want.parameters.bundleName !== globalThis.bundleName) {
+        console.error('Not allowed to start other bundleName Vpn!');
+        return stub;
+      }
+    } else {
+      console.error('bundleName is not set!');
+      return stub;
     }
-    
+
+    this.checkPermission().then(hasPermission => {
+      if (hasPermission) {
+        console.info('No need to get permission again: ' + globalThis.bundleName);
+        return;
+      }
+      appManager.getForegroundApplications().then(data => {
+        if (data &&data.some(item => item.uid === globalThis.vpnCallingUid)) {
+          console.info('Current calling uid is in foreground.');
+          this.showDialog();
+        } else {
+          console.error('App is not in foreground, not allowed to show dialog!');
+          return;
+        }
+      }).catch(err => {
+        console.error(`getForegroundApplications failed! error code: ${err.code}, error msg: ${err.message}`);
+      })
+    });
+    return stub;
+  }
+
+  private async queryValue(dataShareHelper: dataShare.DataShareHelper, uri: string, keyword: string)
+      : Promise<string> {
+    const da = new dataSharePredicates.DataSharePredicates();
+    da.equalTo('KEYWORD', keyword);
+    try {
+      const resultSet = await dataShareHelper.query(uri, da, ['*']);
+      if (!resultSet || !resultSet.goToFirstRow()) {
+        console.error('goToFirstRow failed!');
+        return '';
+      }
+      const columnIndex = resultSet.getColumnIndex('VALUE');
+      return resultSet.getString(columnIndex);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  private async checkPermission(): Promise<boolean> {
+    try {
+      const uri = 'datashare:///com.ohos.settingsdata/entry/settingsdata/SETTINGSDATA?Proxy=true&key=vpnext_mode';
+      const dataShareHelper = await dataShare.createDataShareHelper(this.context, uri);
+      if (!dataShareHelper) {
+        console.error('createDataShareHelper failed!');
+        return false;
+      }
+      const accountManager = osAccount.getAccountManager();
+      const localId = accountManager.getOsAccountLocalIdForUidSync(globalThis.vpnCallingUid);
+      let result = await this.queryValue(dataShareHelper, uri, globalThis.bundleName + '_' + localId);
+      if (result === '1')
+        return true;
+      result = await this.queryValue(dataShareHelper, uri, globalThis.bundleName);
+      return result === '1';
+    } catch (err) {
+      console.error('checkPermission failed!');
+      return false;
+    }
+  }
+
+  private showDialog() : void {
     let dis;
     try {
       dis = display.getDefaultDisplaySync();
@@ -91,8 +169,6 @@ export default class VpnDialogAbility extends extension {
       ctx: this.context
     };
     this.createWindow(windowConfig, navigationBarRect);
-
-    return new VpnDialogStub('VpnRightDialog');
   }
 
   onDisconnect(want: Want): void {
