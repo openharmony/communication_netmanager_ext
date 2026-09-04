9c1edb9e5d06abef36d2858ed74974471d5c61ae
benlau000 <liubin360@huawei.com>
2026-09-04 11:51:45 +0800

    update: 更新文件 eap_hdi_wpa_manager_test.cpp
    Signed-off-by: benlau000 <liubin360@huawei.com>

--- test/ethernetmanager/unittest/ethernet_manager_test/eap_hdi_wpa_manager_test.cpp
+++ test/ethernetmanager/unittest/ethernet_manager_test/eap_hdi_wpa_manager_test.cpp
@@ -169,6 +169,26 @@ HWTEST_F(EapHdiWpaManagerTest, SetEapConfigTest004, TestSize.Level1)
     profile.eapMethod = EapMethod::EAP_TTLS;
     EXPECT_EQ(manager_->SetEapConfig(profile), NETMANAGER_EXT_SUCCESS);
 }
+
+HWTEST_F(EapHdiWpaManagerTest, SetEapConfigTest005, TestSize.Level1)
+{
+    std::shared_ptr<EapHdiWpaManager> manager_ = std::make_shared<EapHdiWpaManager>();
+    EthEapProfile profile;
+    profile.eapMethod = static_cast<EapMethod>(100);
+    EXPECT_EQ(manager_->SetEapConfig(profile), NETMANAGER_EXT_SUCCESS);
+}
+ 
+HWTEST_F(EapHdiWpaManagerTest, SetEapConfigTest006, TestSize.Level1)
+{
+    std::shared_ptr<EapHdiWpaManager> manager_ = std::make_shared<EapHdiWpaManager>();
+    EthEapProfile profile;
+    profile.eapMethod = EapMethod::EAP_PEAP;
+    std::string configPath = std::string(CONFIG_PATH) + "eth_wpa_supplicant.conf";
+    std::remove(configPath.c_str());
+    symlink("/nonexistent_target", configPath.c_str());
+    EXPECT_NE(manager_->SetEapConfig(profile), NETMANAGER_EXT_SUCCESS);
+    std::remove(configPath.c_str());
+}
  
 HWTEST_F(EapHdiWpaManagerTest, AppendIfNotEmptyTest001, TestSize.Level1)
 {
@@ -200,12 +220,33 @@ HWTEST_F(EapHdiWpaManagerTest, AppendCommonTlsParamsTest001, TestSize.Level1)
     EXPECT_FALSE(manager_->setNetCmd_.empty());
 }
 
+HWTEST_F(EapHdiWpaManagerTest, AppendIfNotEmptyTest002, TestSize.Level1)
+{
+    std::shared_ptr<EapHdiWpaManager> manager_ = std::make_shared<EapHdiWpaManager>();
+    manager_->setNetCmd_.clear();
+    manager_->AppendIfNotEmpty("key=", "value\"with\"quotes");
+    EXPECT_FALSE(manager_->setNetCmd_.empty());
+    manager_->setNetCmd_.clear();
+    manager_->AppendIfNotEmpty("key=", "value\nwith\nnewline");
+    EXPECT_FALSE(manager_->setNetCmd_.empty());
+    manager_->setNetCmd_.clear();
+    manager_->AppendIfNotEmpty("key=", "value\rwith\rcr");
+    EXPECT_FALSE(manager_->setNetCmd_.empty());
+}
+
 HWTEST_F(EapHdiWpaManagerTest, Phase2MethodToStrTest002, TestSize.Level1)
 {
     std::shared_ptr<EapHdiWpaManager> manager_ = std::make_shared<EapHdiWpaManager>();
     EXPECT_EQ(manager_->Phase2MethodToStr(EapMethod::EAP_PEAP, Phase2Method::PHASE2_AKA_PRIME), "auth=AKA'");
     EXPECT_EQ(manager_->Phase2MethodToStr(EapMethod::EAP_PEAP, Phase2Method::PHASE2_AKA), "auth=AKA");
     EXPECT_EQ(manager_->Phase2MethodToStr(EapMethod::EAP_TTLS, Phase2Method::PHASE2_GTC), "autheap=GTC");
+}
+
+HWTEST_F(EapHdiWpaManagerTest, Phase2MethodToStrTest003, TestSize.Level1)
+{
+    std::shared_ptr<EapHdiWpaManager> manager_ = std::make_shared<EapHdiWpaManager>();
+    EXPECT_EQ(manager_->Phase2MethodToStr(EapMethod::EAP_PEAP, static_cast<Phase2Method>(100)), "auth=NONE");
+    EXPECT_EQ(manager_->Phase2MethodToStr(EapMethod::EAP_TTLS, static_cast<Phase2Method>(100)), "auth=NONE");
 }
  
 HWTEST_F(EapHdiWpaManagerTest, RemoveHistoryCtrl001, TestSize.Level1)
@@ -284,6 +325,25 @@ HWTEST_F(EapHdiWpaManagerTest, WriteEapConfigToFile001, TestSize.Level1)
     EXPECT_TRUE(manager_->WriteEapConfigToFile(context));
     context = "test2";
     EXPECT_TRUE(manager_->WriteEapConfigToFile(context));
+}
+
+HWTEST_F(EapHdiWpaManagerTest, WriteEapConfigToFile002, TestSize.Level1)
+{
+    std::shared_ptr<EapHdiWpaManager> manager_ = std::make_shared<EapHdiWpaManager>();
+    std::string configPath = std::string(CONFIG_PATH) + "eth_wpa_supplicant.conf";
+    std::remove(configPath.c_str());
+    symlink("/nonexistent_target", configPath.c_str());
+    EXPECT_FALSE(manager_->WriteEapConfigToFile("test"));
+    std::remove(configPath.c_str());
+}
+ 
+HWTEST_F(EapHdiWpaManagerTest, WriteEapConfigToFile003, TestSize.Level1)
+{
+    std::shared_ptr<EapHdiWpaManager> manager_ = std::make_shared<EapHdiWpaManager>();
+    std::error_code ec;
+    std::filesystem::remove_all(CONFIG_PATH, ec);
+    EXPECT_FALSE(manager_->WriteEapConfigToFile("test"));
+    std::filesystem::create_directories(CONFIG_PATH, ec);
 }
  
 HWTEST_F(EapHdiWpaManagerTest, OnEapEventReport001, TestSize.Level1)
