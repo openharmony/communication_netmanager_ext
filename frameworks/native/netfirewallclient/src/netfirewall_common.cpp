d6ebb330f4c5c5d1e45aadf075bed93943947739
huang-haitao <huanghaitao16@h-partners.com>
2026-09-18 12:38:56 +0800

    update: 更新文件 netfirewall_common.cpp
    Signed-off-by: huang-haitao <huanghaitao16@h-partners.com>

--- frameworks/native/netfirewallclient/src/netfirewall_common.cpp
+++ frameworks/native/netfirewallclient/src/netfirewall_common.cpp
@@ -311,6 +311,16 @@ sptr<TrafficFilterIPCidr> TrafficFilterIPCidr::Unmarshalling(Parcel &parcel)
         NETMGR_EXT_LOG_E("Read prefixLen failed");
         return nullptr;
     }
+    if (ptr->base_.family_ == static_cast<int32_t>(TrafficFilterIPFamily::IP_FAMILY_V4) &&
+        ptr->prefixLen_ > IPV4_MASK_MAX) {
+        NETMGR_EXT_LOG_E("Read invalid IPv4 prefixLen");
+        return nullptr;
+    }
+    if (ptr->base_.family_ == static_cast<int32_t>(TrafficFilterIPFamily::IP_FAMILY_V6) &&
+        ptr->prefixLen_ > IPV6_MASK_MAX) {
+        NETMGR_EXT_LOG_E("Read invalid IPv6 prefixLen");
+        return nullptr;
+    }
     return ptr;
 }
 
@@ -868,6 +878,10 @@ sptr<TrafficFilterRedirectRule> TrafficFilterRedirectRule::Unmarshalling(Parcel 
         NETMGR_EXT_LOG_E("Read hookPoint failed");
         return nullptr;
     }
+    if (ptr->hookPoint_ < static_cast<int32_t>(TrafficFilterHookPoint::HOOK_INPUT) ||
+        ptr->hookPoint_ > static_cast<int32_t>(TrafficFilterHookPoint::HOOK_POSTROUTING)) {
+        return nullptr;
+    }
 
     if (!parcel.ReadUint8(ptr->protocol_)) {
         NETMGR_EXT_LOG_E("Read protocol failed");
@@ -1161,6 +1175,10 @@ sptr<TrafficFilterPacketRule> TrafficFilterPacketRule::Unmarshalling(Parcel &par
     }
 
     if (!parcel.ReadInt32(ptr->hookPoint_)) {
+        return nullptr;
+    }
+    if (ptr->hookPoint_ < static_cast<int32_t>(TrafficFilterHookPoint::HOOK_INPUT) ||
+        ptr->hookPoint_ > static_cast<int32_t>(TrafficFilterHookPoint::HOOK_POSTROUTING)) {
         return nullptr;
     }
 
